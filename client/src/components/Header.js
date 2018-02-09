aad67d06c17af6a139f693e6b47c4691ca7da984
Resdan Logan <rezn5447@gmail.com>
2018-02-09 11:23:50 -0800

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
