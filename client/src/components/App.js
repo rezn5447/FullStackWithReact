7ed891e05e71d3916d24509c3bd280fb5662393a
Resdan Logan <rezn5447@gmail.com>
2018-02-08 20:30:21 -0800

    moved client out of server folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurbeyNew</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
@@ -11,8 +11,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
