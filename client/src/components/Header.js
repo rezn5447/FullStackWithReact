9ab00bab88d4703c8d86d9be0751c0a10e4a1526
Resdan Logan <rezn5447@gmail.com>
2018-02-09 19:54:45 -0800

    redirect to surveys

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return 'logged in';
+    }
+  }
+
   render() {
-    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
@@ -23,14 +33,3 @@ function mapStateToProps({ auth }) {
   return { auth };
 }
 export default connect(mapStateToProps)(Header);
-// <nav>
-//   <div class="nav-wrapper">
-//     <a href="#" class="brand-logo">Logo</a>
-//     <ul id="nav-mobile" class="right hide-on-med-and-down">
-//       <li><a href="sass.html">Sass</a></li>
-//       <li><a href="badges.html">Components</a></li>
-//       <li><a href="collapsible.html">JavaScript</a></li>
-//     </ul>
-//   </div>
-// </nav>
-//
