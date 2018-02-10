b6f7ba08f6515c10c3f55af5cea48acb590d62b2
Resdan Logan <rezn5447@gmail.com>
2018-02-09 17:32:55 -0800

    redux logger and update app component

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
