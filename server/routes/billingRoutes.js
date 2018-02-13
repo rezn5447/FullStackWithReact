15d3115a5238d7bafb8b5138239e7774157463d9
Resdan Logan <rezn5447@gmail.com>
2018-02-13 12:51:20 -0800

    create own middleware

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,10 @@ var stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+    }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
@@ -12,5 +16,6 @@ module.exports = app => {
 
     req.user.credits += 5;
     const user = await req.user.save();
+    res.send(user);
   });
 };
