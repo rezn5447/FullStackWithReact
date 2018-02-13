cb530f386292adc326f00f8e4dddc567a0570ae5
Resdan Logan <rezn5447@gmail.com>
2018-02-13 12:32:28 -0800

    billing charge schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 // two args means loading something into it
 mongoose.model('users', userSchema);
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,6 +1,14 @@
-const keys = require('../config/keys');
+const keys = require('../../config/keys');
 var stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 fopr 5 credits',
+      source: req.body.id
+    });
+    console.log(charge);
+  });
 };
