ba18af7fee390cdac7003b95011434f21c4c0dd2
Resdan Logan <rezn5447@gmail.com>
2018-02-13 09:35:52 -0800

    create simple credits button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,16 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button type="button" className="btn" style={{ marginRight: '10px' }}>
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
